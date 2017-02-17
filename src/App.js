c5527e2e6cf19d0740d91efcf83fe736f7eee05d
helfi92 <helfi92@gmail.com>
2017-02-17 11:00:45 -0500

    Change App to a stateless functinal component and add linting preset

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Neutrino</h1>
-        <p>Welcome to React</p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <h1>Neutrino</h1>
+    <p>Welcome to React</p>
+  </div>
+);
 
 export default App;
